4603ee040ee0289abf9df0a1bc005c82c1b7b5e7
beans <ujt2h8@gmail.com>
2019-07-14 21:36:18 +0800

    test : should_multiple_cars_when_park_to_parking_lot_then_get_them_back

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -6,7 +6,7 @@ import sun.security.krb5.internal.Ticket;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ParkingBoyTest {
-    public void should_not_featch_car_when_ticket_is_wrong(){
+    public void should_not_featch_car_when_ticket_is_wrong() {
         //given
         Car car = new Car();
         ParkingLot parkingLot = new ParkingLot();
@@ -18,12 +18,12 @@ public class ParkingBoyTest {
         parkingBoy.park(car);
 
         //then
-        Assertions.assertThrows(Exception.class, ()->{
+        Assertions.assertThrows(Exception.class, () -> {
             parkingBoy.fetch(wrongTicket);
         });
     }
 
-    public void should_return_when_park_car_to_parking_lot_then_get_it_back(){
+    public void should_return_when_park_car_to_parking_lot_then_get_it_back() {
         //given
         Car car = new Car();
         ParkingLot parkingLot = new ParkingLot();
@@ -38,6 +38,31 @@ public class ParkingBoyTest {
         //then
         assertSame(car, fetchCar);
 
+    }
+
+    public void should_multiple_cars_when_park_to_parking_lot_then_get_them_back() {
+        //given
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+
+        //when
+        Ticket firstTicket = parkingBoy.park(firstCar);
+        Car fetchFirstCar = parkingBoy.fetch((firstTicket));
+
+
+        Ticket secondTicket = parkingBoy.park(secondCar);
+        Car fetchSecondCar = parkingBoy.fetch((secondTicket));
+
+
+        //then
+
+        assertSame(firstCar, fetchFirstCar);
+        assertSame(secondCar, fetchSecondCar);
     }
 
 
