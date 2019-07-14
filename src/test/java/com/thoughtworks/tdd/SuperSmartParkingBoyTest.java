90d618d831c54a3860c202a3fffa6302f1e3ac3e
beans <ujt2h8@gmail.com>
2019-07-15 06:44:01 +0800

    test : should_park_car_to_the_parkingLot_with_larger_available_positions_rate

---
+++ src/test/java/com/thoughtworks/tdd/SuperSmartParkingBoyTest.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    public void should_park_car_to_the_parkingLot_with_larger_available_positions_rate() throws Exception{
+//given
+        ParkingLot parkingLotOne = new ParkingLot(20);
+        ParkingLot parkingLotTwo = new ParkingLot(10);
+        SuperSmartParkingBoy superSmartParkingBoy = new SmartParkingBoy(parkingLotOne,parkingLotTwo);
+        Car car = new Car();
+
+        //when
+
+        for(int i = 0;i<14;i++){
+            superSmartParkingBoy.park(new Car());
+        }
+
+
+        //then
+        assertSame(12,parkingLotOne.getSize());
+        assertSame(2, parkingLotTwo.getSize());
+
+
+
+    }
+}
